423c8c6ed694420c3b108fd5cf6e15ae7e95b897
JOHN ALAFIATAYO <81050461+splashray@users.noreply.github.com>
2022-11-17 09:18:13 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+const dotenv = require ('dotenv')
+dotenv.config()
+
+const config = {
+    PORT : process.env.PORT || 3002,
+    MONGODB_URI: process.env.MONGODB_URI,
+  
+
+}
+module.exports = config
